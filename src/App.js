64db2ac1329fab3471caed4df0da185a6a485434
Beatriz Aliaga <baliagar@est.emi.edu.bo>
2022-05-20 14:22:35 -0400

    segundo componente

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <>
+    /**helper/note/active */
       <div class="card">
         <div class="card-header red">
           <div class="tittle">
@@ -19,9 +20,29 @@ function App() {
         </div>
         <div class="card-footer">
           <p>Last Updated</p>
-          <p>14/07/2021</p>
+          <p>3/8/2020</p>
         </div>
       </div>
+    /**helper/note/future */
+      <div class="card">
+        <div class="card-header purple">
+          <div class="tittle">
+            <div class="circle">1</div>
+            <p>Agenda/Topic</p>
+          </div>
+          <div class="id">
+            <p>SAAS-000</p>
+          </div>
+        </div>
+        <div class="card-section">
+          <p>Description of what is done here</p>
+        </div>
+        <div class="card-footer">
+          <p>Last Updateds</p>
+          <p>3/8/2020</p>
+        </div>
+      </div>
+
 
     </>
   );

84c39f619390af27179c97f78aaa3e58f582b969
Beatriz Aliaga <baliagar@est.emi.edu.bo>
2022-05-20 14:03:21 -0400

    primer componente

---
+++ src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div class="card">
+        <div class="card-header red">
+          <div class="tittle">
+            <div class="circle">1</div>
+            <p>Agenda/Topic</p>
+          </div>
+          <div class="id">
+            <p>SAAS-000</p>
+          </div>
+        </div>
+        <div class="card-section">
+          <p>Description of what is done here</p>
+        </div>
+        <div class="card-footer">
+          <p>Last Updated</p>
+          <p>14/07/2021</p>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default App;
